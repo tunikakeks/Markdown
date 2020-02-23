d37e7fb071abaaf874aec7b0547ec141996e66de
NikiNikOfficially <mc25.studio@gmail.com>
2020-02-23 17:57:33 +0100

    Added Blockquotes

--- assets/js/markdown.js
+++ assets/js/markdown.js
@@ -14,25 +14,26 @@ function markDown() {
     for (var i = 0; i < ca.length; i++) {
         var data = ca[i].innerHTML
 
-        data = data.replace(/\\\|\|(.*?)\|\|/i, "<p>||$1||</p>")
-        data = data.replace(/\\\*\*(.*?)\*\*/i, "<p>**$1**</p>")
-        data = data.replace(/\\\*\*\*(.*?)\*\*\*/i, "<p>***$1***</p>")
-        data = data.replace(/\\\*(.*?)\*/i, "<p>*$1*</p>")
-        data = data.replace(/\\_(.*?)_/i, "<p>_$1_</p>")
-        data = data.replace(/\\__(.*?)__/i, "<p>__$1__</p>")
-        data = data.replace(/\\~~(.*?)~~/i, "<p>~~$1~~</p>")
-        data = data.replace(/\\``(.*?)``/i, "<p>``$1``</p>")
+        data = data.replace(/\\\|\|(.*?)\|\|/g, "<p>||$1||</p>")
+        data = data.replace(/\\\*\*(.*?)\*\*/g, "<p>**$1**</p>")
+        data = data.replace(/\\\*\*\*(.*?)\*\*\*/g, "<p>***$1***</p>")
+        data = data.replace(/\\\*(.*?)\*/g, "<p>*$1*</p>")
+        data = data.replace(/\\_(.*?)_/g, "<p>_$1_</p>")
+        data = data.replace(/\\__(.*?)__/g, "<p>__$1__</p>")
+        data = data.replace(/\\~~(.*?)~~/g, "<p>~~$1~~</p>")
+        data = data.replace(/\\``(.*?)``/g, "<p>``$1``</p>")
         data = data.replace(/\\(.*?)```\n([^´]+)\n```/gm, '<p>```$2```</p>')
 
-        data = data.replace(/\|\|(.*?)\|\|/i, "<span class='spoiler hidden'>$1</span>") // spoiler
-        data = data.replace(/\*\*(.*?)\*\*/i, "<b>$1</b>") //Bold
-        data = data.replace(/\*\*\*(.*?)\*\*\*/i, "<b><i>$1</i></b>") //bold italic
-        data = data.replace(/\*(.*?)\*/i, "<i>$1</i>") //italic
-        data = data.replace(/_(.*?)_/i, "<i>$1</i>") //italic
-        data = data.replace(/__(.*?)__/i, "<u>$1</u>") // underlined
-        data = data.replace(/~~(.*?)~~/i, "<strike>$1</strike>") // striked
-        data = data.replace(/``(.*?)``/i, '<span class="oneLineBlock">$1</span>') // block
+        data = data.replace(/\|\|(.*?)\|\|/g, "<span class='spoiler hidden'>$1</span>") // spoiler
+        data = data.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>") //Bold
+        data = data.replace(/\*\*\*(.*?)\*\*\*/g, "<b><i>$1</i></b>") //bold italic
+        data = data.replace(/\*(.*?)\*/g, "<i>$1</i>") //italic
+        data = data.replace(/_(.*?)_/g, "<i>$1</i>") //italic
+        data = data.replace(/__(.*?)__/g, "<u>$1</u>") // underlined
+        data = data.replace(/~~(.*?)~~/g, "<strike>$1</strike>") // striked
+        data = data.replace(/``(.*?)``/g, '<span class="oneLineBlock">$1</span>') // block
         data = data.replace(/(.*?)```\n([^´]+)\n```/gm, '<span class="multiLineBlock">$2</span>') // multi line block
+        data = data.replace(/> (.*[^< >])/g, '<span class="blockquote">$1</span>') // Blockquote
 
         data = data.replace(/(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/g, '<span class="uri"><a href="$1" target="_blank">$1</a></span>') // url
 
